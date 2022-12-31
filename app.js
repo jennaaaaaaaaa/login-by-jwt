3863cc1a4e458e253fb173c13cd6351af4d02538
Kim gimin <ggm4954@gmail.com>
2022-12-31 18:59:33 +0900

    second

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-//jwt
+const express = require('express')
+const app = express()
+const port = 1000
+
+app.get('/users', (req,res) => {
+    res.send('users')
+})
+
+app.post('/login', (req,res) => {
+    res.send('login')
+})
+
+app.post('/logout', (req,res) => {
+    res.send('logout')
+})
+
+app.post('/register', (req,res) => {
+    res.send('register')
+})
+
+
+app.listen(port, () => {
+    console.log(port, "서버 연결")
+})
